e2e116e0f4d1ee9ad46b73b0a9fd44c307c04078
Lionel Cancellieri <lio.cancellieri@gmail.com>
2025-05-26 14:39:11 -0300

    Updated: Mostrar gifs en pantalla: PC

--- src/app/gifs/components/gif-list/gif-list.component.ts
+++ src/app/gifs/components/gif-list/gif-list.component.ts
@@ -1,12 +1,12 @@
 import { Component, input } from '@angular/core';
 import { GifListItemComponent } from "./gif-list-item/gif-list-item.component";
-import { NgFor } from '@angular/common';
+import { Gif } from '../../interfaces/gif.interface';
 
 @Component({
   selector: 'gif-list',
-  imports: [GifListItemComponent, NgFor],
+  imports: [GifListItemComponent],
   templateUrl: './gif-list.component.html'
 })
 export class GifListComponent {
-      gifs = input.required<string[]>()
+      gifs = input.required<Gif[]>()
  }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,7 @@ export const environment = {
   companyName2: 'App',
   companySlogan: 'Maneja tus gifs',
   // ApiKeys
+  giphyApiKey: 'KVpOWiwFsgW67jn0jMDOCHo3CoULN4G8',
+  giphyUrl: 'https://api.giphy.com/v1',
   // URLS
 };
